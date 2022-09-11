ab376e2bf5c3bf6e4de45461b2f5f99bf7088529
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-11 16:33:31 +0530

    Alphabet vowels

---
+++ AlphabetsVowelOrConsonants.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.corejava;
+
+import java.util.Scanner;
+
+public class AlphabetsVowelOrConsonants {
+	 public static void main(String[] args) {
+	        char c;
+
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter character : ");
+	        c = scanner.next().charAt(0);
+	        if (c =='a' || c =='e' || c =='i' || c =='o' || c =='u' ||c =='A' || c =='E' || c =='I' || c =='O' || c =='U' ) {
+	            System.out.println(c+ " is Vowel");
+	        }
+	        else {
+	            System.out.println(c+ " is Consonant");
+	        }
+	    }
+}
